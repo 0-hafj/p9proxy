50554fbc5ee8cdc344a20fdf90eba0895ec45776
Håvard Fjukstad <havard.fjukstad@dfo.no>
2023-02-20 14:09:48 +0100

    Added env for proxy to local docker container

--- bd.sh
+++ bd.sh
@@ -3,7 +3,7 @@
 docker stop p9proxy
 docker rm p9proxy
 docker build -t tlodevacr.azurecr.io/p9proxy:latest .
-docker run -d --name p9proxy -p 8080:8080 tlodevacr.azurecr.io/p9proxy:latest
+docker run -d --name p9proxy -p 8080:8080 -e HTTP_PROXY="http://localhost:3128" -e HTTPS_PROXY="http://localhost:3128" -e NO_PROXY=".sfso.no, .dfo.no" tlodevacr.azurecr.io/p9proxy:latest
 docker push tlodevacr.azurecr.io/p9proxy:latest
 # docker exec -it -u root p9proxy env
 # docker exec -it -u root p9proxy cat /var/log/squid/access.log
